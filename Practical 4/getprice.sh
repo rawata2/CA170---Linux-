0d65630ff6de072d4ecd10ac45cf2cc8fee8afa4
Arvind <72453093+rawata2@users.noreply.github.com>
2021-04-11 12:22:54 +0100

    Update getprice.sh

--- Practical 4/getprice.sh
+++ Practical 4/getprice.sh
@@ -2,6 +2,6 @@
 
 wget -q -O - http://bigcharts.marketwatch.com/quickchart/quickchart.asp?symb=$1 | 
 tr -d '\r\n' | 
-sed -e "s|.*.Last:</span>||" 
-| sed -e "s|</div>..$||" | 
+sed -e "s|.*.Last:</span>||" | 
+sed -e "s|</div>..$||" | 
 sed -e "s|^..<div>||"
